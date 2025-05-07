d887ff5a43cba38ae81154c9a013cbc9fbbc606e
Romain Heurtaux <zelyok.95@gmail.com>
2025-05-07 12:02:54 +0200

    Push Ech3_Java

---
+++ Ech3c_Java/Piece.java
@@ -0,0 +1,19 @@
+public class Piece extends Case{
+    private int[] position;
+
+    public Piece(int x, int y) {
+        super(x,y);
+        this.position = new int[2];
+        this.position[0] = x;
+        this.position[1] = y;
+    }
+
+    public void move(int x, int y) {
+        this.position[0] = x;
+        this.position[1] = y;
+    }
+
+    function getPosition() {
+        return this.position;
+    }
+}
