62744f5bface2b1cfa45bab12cf24f3f051a101c
Romain Heurtaux <zelyok.95@gmail.com>
2025-06-05 11:46:57 +0200

    Fix 1of2 moves of pawn

--- Ech3c_Java/Pawn.java
+++ Ech3c_Java/Pawn.java
@@ -4,8 +4,8 @@
 public class Pawn extends Piece {
 
     /* Variables */
-    
-    
+
+
     /* Setter & getter */
     public Pawn(int x, int y, String color) {
         super(x, y, "P", color);
@@ -15,11 +15,13 @@ public class Pawn extends Piece {
     boolean is_validMove(int position[], Echiquier chessboard) {
         // The pawn can move one square forward, or two squares forward from its starting position
         int[] cur_pos = this.getPosition();
+        if (this.getCouleur()=="Black"){
+                return (position[0] == cur_pos[0]+1);
+        } else {
+                return (position[0] == cur_pos[0]-1);
+        }
 
-        return (
-
-            (position[0] == cur_pos[0]+1 && position[1] == cur_pos[1] + 1) ||
-            (position[0] == cur_pos[0]+1 && position[1] == cur_pos[1] - 1)
-        );
+        //(position[0] == cur_pos[0]+1 && position[1] == cur_pos[1] + 1) ||
+        //(position[0] == cur_pos[0]+1 && position[1] == cur_pos[1] - 1));
     }
 }
