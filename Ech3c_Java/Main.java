aa998a5f6fbbb9afe4a5b7f00989665b16127c5f
JMLMath <jmlmatheo@icloud.com>
2025-06-05 09:51:05 +0200

    mistake fix

--- Ech3c_Java/Main.java
+++ Ech3c_Java/Main.java
@@ -13,7 +13,7 @@ public class Main {
         while (Game) {
             // -> Afficher l'état du jeu (grille avec les pièces)
             // -> Demander à l'utilisateur de sélectionner une pièce
-            int[][] uer_move;
+            int[][] user_move;
             user_move = screen.actualise();
             // -> Demander à l'utilisateur de sélectionner une destination
             screen.actualise();
